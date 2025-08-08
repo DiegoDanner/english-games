1e253cdf768eb8bc5202b0a6d105a07c128cb54e
DiegoDanner <diegodanner@gmail.com>
2025-08-08 20:39:56 -0300

    Merged Changes

---
+++ meu-quiz/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-center p-4">
+      <h1>Bem-vindo ao Meu Quiz</h1>
+      <a href="/quiz1" className="text-blue-500 underline">Ir para o Quiz 1</a>
+    </div>
+  );
+}
