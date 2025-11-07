ea37dcb02d9ad1b549b774692a704b34705f2dd4
Diego Danner <48870230+DiegoDanner@users.noreply.github.com>
2025-11-06 21:46:40 -0300

    Create firebase-config.js

---
+++ Games/js/firebase-config.js
@@ -0,0 +1,12 @@
+// ARQUIVO DE EXEMPLO DE CONFIGURAÇÃO DO FIREBASE
+// Renomeie este arquivo para "firebase-config.js" e preencha com suas próprias credenciais do Firebase.
+// Este arquivo não deve ser versionado.
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAtK5IZ90-5eAtXKJJW0bx0x0qUjgfoutc",
+    authDomain: "login-danneridiomas.firebaseapp.com",
+    projectId: "login-danneridiomas",
+    storageBucket: "login-danneridiomas.firebasestorage.app",
+    messagingSenderId: "40775533575",
+    appId: "1:40775533575:web:3fff43067b1f4dcba216a2"
+};
